bef4b8811edf844d7d4cbe626d331c5225e9458e
Devan <devan.allara@interapt.com>
2018-03-19 16:38:15 -0400

    created ui

---
+++ Mind Flash/CreateQuestionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreateQuestionViewController.swift
+//  Mind Flash
+//
+//  Created by Devan Allara on 3/19/18.
+//  Copyright © 2018 Devan Allara. All rights reserved.
+//
+
+import UIKit
+
+class CreateQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    
+
+
+}
