fdca2ab713231b23ced705df3026fb2b7d302904
Devan <devan.allara@interapt.com>
2018-04-10 16:07:44 -0400

    finished project

--- Mind Flash/CreateQuestionViewController.swift
+++ Mind Flash/CreateQuestionViewController.swift
@@ -4,11 +4,11 @@
 //
 //  Created by Devan Allara on 3/19/18.
 //  Copyright © 2018 Devan Allara. All rights reserved.
-//
 
 import UIKit
 
 class CreateQuestionViewController: UIViewController {
+    
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var questionTextField: UITextField!
@@ -18,9 +18,11 @@ class CreateQuestionViewController: UIViewController {
     @IBOutlet weak var answer4TextField: UITextField!
     @IBOutlet weak var segmentedController: UISegmentedControl!
     
+    var newQuestion: Question!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,11 +34,15 @@ class CreateQuestionViewController: UIViewController {
         self.dismiss(animated: true)
     }
     @IBAction func addButtonTapped(_ sender: Any) {
-        
+       newQuestion = Question(questionText: questionTextField.text!
+            , answers: [answer1TextField.text!, answer2TextField.text!, answer3TextField.text!, answer4TextField.text!], correctAnswer: segmentedController.selectedSegmentIndex)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "guessScreen") {
+            let vc = segue.destination as! GuessViewController
+            vc.questions.append(newQuestion)
+        }
+    }
     
-    
-
-
 }
